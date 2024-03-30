ca942282934cb12b242b4bf997833aacb296abb0
Maxim Chernyshov <chrn.maxim@gmail.com>
2024-03-30 11:13:57 +0300

    Change pub_date validator tolerance

--- blogicum/blog/validators.py
+++ blogicum/blog/validators.py
@@ -8,10 +8,10 @@ TIME_DELTA: int = 10
 
 def post_pub_date(pub_date: timezone) -> None:
     """Validate post pub_date."""
-    formatted_pub_date = pub_date.strftime("%Y-%m-%d %H:%M")
+    formatted_pub_date = pub_date.strftime("%Y-%m-%d")
     formatted_local_time = (
         timezone.localtime() - timezone.timedelta(minutes=10)
-    ).strftime("%Y-%m-%d %H:%M")
+    ).strftime("%Y-%m-%d")
     if formatted_pub_date < formatted_local_time:
         raise ValidationError(
             f'Дата публикации {formatted_pub_date} не может быть раньше, '
